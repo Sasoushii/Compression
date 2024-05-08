dcd736b30850351b38dc309f69c1374b51452332
Joseph Daly <51287094+JanotLeLapin@users.noreply.github.com>
2024-05-08 16:43:31 +0200

    Load actual images

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 import numpy as np
+from PIL import Image
+
+def load_image(file: str) -> np.ndarray[np.uint8]:
+    img = Image.open(file)
+    img.load()
+
+    data = np.asarray(img, dtype=np.uint8)
+    return data
+
+def save_image(file: str, data: np.ndarray[np.uint8]):
+    img = Image.fromarray(data.astype(np.uint8), mode="RGB")
+    img.save(file)
+
+def padded_shape(shape: tuple[int]) -> tuple[int]:
+    res = [n + 4 - n % 4 for n in shape]
+    res[2] = 3
+    return tuple(res)
 
 def add_padding(data: np.ndarray[np.uint8]) -> np.ndarray[np.uint8]:
     """
     Ajoute du padding sur les octets qui représentent une image, afin que ses dimensions soient multiples de 4
     """
 
-    (h, w) = data.shape
-    fixed_w = 4 - (w % 4)
-
-    data = np.hstack((data, np.zeros((h, fixed_w))))
-    data = np.vstack((data, np.zeros((4 - (h % 4), w + fixed_w))))
-    return data
+    (h, w, _) = data.shape
+    result = np.zeros(padded_shape(data.shape))
+    result[0:h, 0:w, 0:3] = data
+    return result
 
 def remove_padding(data: np.ndarray[np.uint8], shape: tuple[int, int]) -> np.ndarray[np.uint8]:
     """
     Retire le padding des octets qui représentent une image.
     """
 
-    (h, w) = shape
-
+    (h, w, _) = shape
     return data[0:h, 0:w]
 
 def split(data: np.ndarray[np.uint8]) -> list[np.ndarray[np.uint8]]: 
@@ -26,7 +40,7 @@ def split(data: np.ndarray[np.uint8]) -> list[np.ndarray[np.uint8]]:
     Découpe les octets qui représentent une image en des blocs de forme 4x4
     """
 
-    (h, w) = data.shape
+    (h, w, _) = data.shape
     
     res = []
     for x in range(0, w, 4):
@@ -40,9 +54,9 @@ def join(blocks: list[np.ndarray[np.uint8]], shape: tuple[int, int]) -> np.ndarr
     Reconstitue les octets qui représentent une image à partir de blocs de forme 4x4
     """
 
-    [h, w] = map(lambda n: n + 4 - n % 4, shape)
+    (h, w, _) = padded_shape(shape)
 
-    data = np.zeros((h, w))
+    data = np.zeros((h, w, 3))
 
     for x in range(0, w, 4):
         for y in range(0, h, 4):
@@ -51,15 +65,10 @@ def join(blocks: list[np.ndarray[np.uint8]], shape: tuple[int, int]) -> np.ndarr
 
     return data
 
-mat = np.array([
-    [1, 2, 3, 4, 5],
-    [6, 7, 8, 9, 10],
-    [11, 12, 13, 14, 15],
-], np.uint8)
+mat = load_image("image.jpg")
 shape = mat.shape
 
 blocks = split(add_padding(mat))
-print(blocks)
+removed = remove_padding(join(blocks, shape), shape)
 
-original = remove_padding(join(blocks, shape), shape)
-print(original)
+save_image("output.jpg", removed)
