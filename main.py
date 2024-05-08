56cc5aa5c7d87da614a0e979a4ef2ce32ff6540d
Sabri Kabani <sabri.kabani@ens.uvsq.fr>
2024-05-08 16:12:22 +0200

    maj

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
+
+
+import PIL
+from PIL import Image
 import numpy as np
+import scipy as sp
+import os
+from math import log10, sqrt
+
+def load(filename):
+    toLoad= Image.open(filename)
+    return np.array(toLoad)
+
+
+def psnr(original, compressed):
+    mse = np.mean((original.astype(int) - compressed) ** 2)
+    max_pixel = 255.0
+    psnr = 20 * log10(max_pixel / sqrt(mse))
+    return psnr
+#---------START-------------
 
 def add_padding(data: np.ndarray[np.uint8]) -> np.ndarray[np.uint8]:
     """
@@ -32,3 +51,19 @@ padded = add_padding(mat)
 print(padded)
 
 print(remove_padding(padded, shape))
+
+#EXERCICE 4:
+def tronque(n,p):
+    #conversion n en bits
+    i=0
+    sum=""
+    while n>0:
+        sum+=str(n%2)
+        n=n//2
+        i+=1
+    sum=sum[::-1]
+    if p!=0:
+        sum=sum[:-p]
+    return(sum)
+
+#-----------SECTION TEST------------------------
